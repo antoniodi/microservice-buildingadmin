237dac7c3a272a8dba89039ae16a1d1201748208
antoniococr <anthonydicortes@gmail.com>
2020-03-12 19:43:41 -0500

    new files with java 11+

---
+++ src/test/java/com/org/buildingadmin/BuildingadminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.buildingadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildingadminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
